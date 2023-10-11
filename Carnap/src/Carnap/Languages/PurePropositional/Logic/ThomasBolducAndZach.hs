c0a446fb53e1df5ef1532fa833b57fbcc8b46f8b
Christian Pardillo Laursen <christiansfd@protonmail.com>
2023-10-11 16:26:16 +0100

    Reset ThomasBolducAndZach

--- Carnap/src/Carnap/Languages/PurePropositional/Logic/ThomasBolducAndZach.hs
+++ Carnap/src/Carnap/Languages/PurePropositional/Logic/ThomasBolducAndZach.hs
@@ -42,95 +42,87 @@ data ThomasBolducAndZachTFLCore = ConjIntro  | As
                                 deriving (Eq)
 
 data ThomasBolducAndZachTFL = Core ThomasBolducAndZachTFLCore
-                            | DisSyllo1    | DisSyllo2
-                            | ModTollens   | DoubleNeg  
-                            | DoubleNegIntro
-                            | Lem1         | Lem2
-                            | Lem3         | Lem4
-                            | DeMorgan1    | DeMorgan2
-                            | DeMorgan3    | DeMorgan4
-                            | NegBicoDist1 | NegBicoDist2 
-                            | BicoMT1      | BicoMT2
+                            | DisSyllo1  | DisSyllo2
+                            | ModTollens | DoubleNeg  
+                            | Lem1       | Lem2
+                            | Lem3       | Lem4
+                            | DeMorgan1  | DeMorgan2
+                            | DeMorgan3  | DeMorgan4
                             deriving (Eq)
                             --skipping derived rules for now
 
 instance Show ThomasBolducAndZachTFLCore where
-        show ConjIntro    = "∧I"
-        show ConjElim1    = "∧E"
-        show ConjElim2    = "∧E"
-        show NegeIntro1   = "¬I"
-        show NegeIntro2   = "¬I"
-        show NegeElim     = "¬E"
-        show Indirect1    = "IP"
-        show Indirect2    = "IP"
-        show CondIntro1   = "→I"
-        show CondIntro2   = "→I"
-        show CondElim     = "→E"
-        show ContElim     = "X"
-        show DisjElim1    = "∨E"
-        show DisjElim2    = "∨E"
-        show DisjElim3    = "∨E"
-        show DisjElim4    = "∨E"
-        show DisjIntro1   = "∨I"
-        show DisjIntro2   = "∨I"
-        show BicoIntro1   = "↔I"
-        show BicoIntro2   = "↔I"
-        show BicoIntro3   = "↔I"
-        show BicoIntro4   = "↔I"
-        show BicoElim1    = "↔E"
-        show BicoElim2    = "↔E"
-        show Reiterate    = "R"
-        show As           = "AS"
-        show (Pr _)       = "PR"
+        show ConjIntro  = "∧I"
+        show ConjElim1  = "∧E"
+        show ConjElim2  = "∧E"
+        show NegeIntro1 = "¬I"
+        show NegeIntro2 = "¬I"
+        show NegeElim   = "¬E"
+        show Indirect1  = "IP"
+        show Indirect2  = "IP"
+        show CondIntro1 = "→I"
+        show CondIntro2 = "→I"
+        show CondElim   = "→E"
+        show ContElim   = "X"
+        show DisjElim1  = "∨E"
+        show DisjElim2  = "∨E"
+        show DisjElim3  = "∨E"
+        show DisjElim4  = "∨E"
+        show DisjIntro1 = "∨I"
+        show DisjIntro2 = "∨I"
+        show BicoIntro1 = "↔I"
+        show BicoIntro2 = "↔I"
+        show BicoIntro3 = "↔I"
+        show BicoIntro4 = "↔I"
+        show BicoElim1  = "↔E"
+        show BicoElim2  = "↔E"
+        show Reiterate  = "R"
+        show As         = "AS"
+        show (Pr _)     = "PR"
 
 instance Show ThomasBolducAndZachTFL where
-        show (Core x)     = show x
-        show DisSyllo1    = "DS"
-        show DisSyllo2    = "DS"
-        show ModTollens   = "MT"
-        show DoubleNeg = "DNE"
-        show DoubleNegIntro= "DNI"
-        show Lem1         = "LEM"
-        show Lem2         = "LEM"
-        show Lem3         = "LEM"
-        show Lem4         = "LEM"
-        show DeMorgan1    = "DeM"
-        show DeMorgan2    = "DeM"
-        show DeMorgan3    = "DeM"
-        show DeMorgan4    = "DeM"
-        show NegBicoDist1 = "NBD"
-        show NegBicoDist2 = "NBD"
-        show BicoMT1      = "BMT"
-        show BicoMT2      = "BMT"
+        show (Core x)   = show x
+        show DisSyllo1  = "DS"
+        show DisSyllo2  = "DS"
+        show ModTollens = "MT"
+        show DoubleNeg  = "DNE"
+        show Lem1       = "LEM"
+        show Lem2       = "LEM"
+        show Lem3       = "LEM"
+        show Lem4       = "LEM"
+        show DeMorgan1  = "DeM"
+        show DeMorgan2  = "DeM"
+        show DeMorgan3  = "DeM"
+        show DeMorgan4  = "DeM"
 
 instance Inference ThomasBolducAndZachTFLCore PurePropLexicon (Form Bool) where
-        ruleOf ConjIntro    = adjunction
-        ruleOf ConjElim1    = simplificationVariations !! 0
-        ruleOf ConjElim2    = simplificationVariations !! 1
-        ruleOf NegeIntro1   = constructiveFalsumReductioVariations !! 0
-        ruleOf NegeIntro2   = constructiveFalsumReductioVariations !! 1
-        ruleOf NegeElim     = falsumIntroduction
-        ruleOf Indirect1    = nonConstructiveFalsumReductioVariations !! 0
-        ruleOf Indirect2    = nonConstructiveFalsumReductioVariations !! 1
-        ruleOf CondIntro1   = conditionalProofVariations !! 0
-        ruleOf CondIntro2   = conditionalProofVariations !! 1
-        ruleOf CondElim     = modusPonens
-        ruleOf ContElim     = falsumElimination
-        ruleOf DisjIntro1   = additionVariations !! 0
-        ruleOf DisjIntro2   = additionVariations !! 1 
-        ruleOf DisjElim1    = proofByCasesVariations !! 0
-        ruleOf DisjElim2    = proofByCasesVariations !! 1
-        ruleOf DisjElim3    = proofByCasesVariations !! 2
-        ruleOf DisjElim4    = proofByCasesVariations !! 3
-        ruleOf BicoIntro1   = biconditionalProofVariations !! 0
-        ruleOf BicoIntro2   = biconditionalProofVariations !! 1
-        ruleOf BicoIntro3   = biconditionalProofVariations !! 2
-        ruleOf BicoIntro4   = biconditionalProofVariations !! 3
-        ruleOf BicoElim1    = biconditionalPonensVariations !! 0
-        ruleOf BicoElim2    = biconditionalPonensVariations !! 1
-        ruleOf Reiterate    = identityRule
-        ruleOf As           = axiom
-        ruleOf (Pr _)       = axiom
+        ruleOf ConjIntro  = adjunction
+        ruleOf ConjElim1  = simplificationVariations !! 0
+        ruleOf ConjElim2  = simplificationVariations !! 1
+        ruleOf NegeIntro1 = constructiveFalsumReductioVariations !! 0
+        ruleOf NegeIntro2 = constructiveFalsumReductioVariations !! 1
+        ruleOf NegeElim   = falsumIntroduction
+        ruleOf Indirect1  = nonConstructiveFalsumReductioVariations !! 0
+        ruleOf Indirect2  = nonConstructiveFalsumReductioVariations !! 1
+        ruleOf CondIntro1 = conditionalProofVariations !! 0
+        ruleOf CondIntro2 = conditionalProofVariations !! 1
+        ruleOf CondElim   = modusPonens
+        ruleOf ContElim   = falsumElimination
+        ruleOf DisjIntro1 = additionVariations !! 0
+        ruleOf DisjIntro2 = additionVariations !! 1 
+        ruleOf DisjElim1  = proofByCasesVariations !! 0
+        ruleOf DisjElim2  = proofByCasesVariations !! 1
+        ruleOf DisjElim3  = proofByCasesVariations !! 2
+        ruleOf DisjElim4  = proofByCasesVariations !! 3
+        ruleOf BicoIntro1 = biconditionalProofVariations !! 0
+        ruleOf BicoIntro2 = biconditionalProofVariations !! 1
+        ruleOf BicoIntro3 = biconditionalProofVariations !! 2
+        ruleOf BicoIntro4 = biconditionalProofVariations !! 3
+        ruleOf BicoElim1  = biconditionalPonensVariations !! 0
+        ruleOf BicoElim2  = biconditionalPonensVariations !! 1
+        ruleOf Reiterate  = identityRule
+        ruleOf As         = axiom
+        ruleOf (Pr _)     = axiom
 
         indirectInference x
             | x `elem` [ DisjElim1, DisjElim2, DisjElim3, DisjElim4
@@ -170,7 +162,6 @@ instance Inference ThomasBolducAndZachTFL PurePropLexicon (Form Bool) where
         ruleOf DisSyllo2  = modusTollendoPonensVariations !! 1
         ruleOf ModTollens = modusTollens
         ruleOf DoubleNeg  = doubleNegationElimination
-        ruleOf DoubleNegIntro = doubleNegationIntroduction
         ruleOf Lem1       = tertiumNonDaturVariations !! 0 
         ruleOf Lem2       = tertiumNonDaturVariations !! 1
         ruleOf Lem3       = tertiumNonDaturVariations !! 2
@@ -179,10 +170,6 @@ instance Inference ThomasBolducAndZachTFL PurePropLexicon (Form Bool) where
         ruleOf DeMorgan2  = deMorgansLaws !! 1
         ruleOf DeMorgan3  = deMorgansLaws !! 2
         ruleOf DeMorgan4  = deMorgansLaws !! 3
-        ruleOf NegBicoDist1 = negatedBiconditionalVariations !! 0
-        ruleOf NegBicoDist2 = negatedBiconditionalVariations !! 1
-        ruleOf BicoMT1    = biconditionalTollensVariations !! 0
-        ruleOf BicoMT2    = biconditionalTollensVariations !! 1
 
         indirectInference (Core x) = indirectInference x
         indirectInference x
@@ -217,8 +204,8 @@ instance Inference ThomasBolducAndZachTFL PurePropLexicon (Form Bool) where
 parseThomasBolducAndZachTFLCore :: RuntimeDeductionConfig PurePropLexicon (Form Bool) -> Parsec String u [ThomasBolducAndZachTFLCore]
 parseThomasBolducAndZachTFLCore rtc = do r <- choice (map (try . string) [ "AS","PR","&I","/\\I", "∧I","&E","/\\E","∧E", "~I","-I", "¬I"
                                                                       , "~E","-E", "¬E","IP","->I", ">I", "=>I", "→I","->E", "=>E", ">E", "→E", "X"
-                                                                      , "vI","\\/I", "|I", "∨I", "vE","\\/E", "|E", "∨E","<->I", "↔I","<->E", "↔E"
-                                                                      , "R"])
+                                                                      , "vI","\\/I", "|I", "∨I", "vE","\\/E", "|E", "∨E","<->I", "↔I","<->E"
+                                                                      , "↔E","R"])
                                          case r of
                                             r | r == "AS"   -> return [As]
                                               | r == "PR" -> return [Pr (problemPremises rtc)]
@@ -238,16 +225,13 @@ parseThomasBolducAndZachTFLCore rtc = do r <- choice (map (try . string) [ "AS",
 
 parseThomasBolducAndZachTFL :: RuntimeDeductionConfig PurePropLexicon (Form Bool) -> Parsec String u [ThomasBolducAndZachTFL]
 parseThomasBolducAndZachTFL rtc = try parseExt <|> (map Core <$> parseThomasBolducAndZachTFLCore rtc)
-        where parseExt = do r <- choice (map (try . string) ["DS","MT","DNE", "DNI", "LEM","DeM", "NBD", "BMT"])
+        where parseExt = do r <- choice (map (try . string) ["DS","MT","DNE","LEM","DeM"])
                             case r of
                                r | r == "DS"   -> return [DisSyllo1,DisSyllo2]
                                  | r == "MT"   -> return [ModTollens]
                                  | r == "DNE"  -> return [DoubleNeg]
-                                 | r == "DNI"  -> return [DoubleNegIntro]
                                  | r == "LEM"  -> return [Lem1,Lem2,Lem3,Lem4]
                                  | r == "DeM"   -> return [DeMorgan1,DeMorgan2,DeMorgan3,DeMorgan4]
-                                 | r == "NBD"  -> return [NegBicoDist1, NegBicoDist2]
-                                 | r == "BMT"  -> return [BicoMT1, BicoMT2]
 
 parseThomasBolducAndZachTFLCoreProof :: RuntimeDeductionConfig PurePropLexicon (Form Bool) -> String -> [DeductionLine ThomasBolducAndZachTFLCore PurePropLexicon (Form Bool)]
 parseThomasBolducAndZachTFLCoreProof rtc = toDeductionFitch (parseThomasBolducAndZachTFLCore rtc) (purePropFormulaParser thomasBolducZachOpts) 
